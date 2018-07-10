05882841f575ffe83cc2649cadd49f7949e8802b
Rob Poyck <rob.poyck@streetscooter.eu>
2018-07-10 16:53:42 +0200

    Filled in known sections

--- src/MPC.h
+++ src/MPC.h
@@ -2,19 +2,21 @@
 #define MPC_H
 
 #include <vector>
+#include <cppad/cppad.hpp>
+#include <cppad/ipopt/solve.hpp>
 #include "Eigen-3.3/Eigen/Core"
 
 using namespace std;
 
 class MPC {
- public:
-  MPC();
+    public:
+	MPC();
 
-  virtual ~MPC();
+	virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+	// Solve the model given an initial state and polynomial coefficients.
+	// Return the first actuatotions.
+	vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
